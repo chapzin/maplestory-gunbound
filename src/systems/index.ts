c3e35f21062f5ffb38d207cb6d1fe21f8203bbbf
Ricardo Godoi <chapzin@gmail.com>
2025-03-29 15:15:07 -0300

    Sistema de rede e organização dos sistemas: implementação do sistema multijogador e exportação centralizada de sistemas

---
+++ src/systems/index.ts
@@ -0,0 +1,6 @@
+// Exportações dos sistemas
+export { PhysicsSystem } from './physics-adapter';
+export { TerrainSystem } from './terrain-adapter';
+export { InputSystem } from './input/input-system';
+export { EventSystem } from './event-system';
+export { ExplosionEffect } from './effects/explosion'; 
